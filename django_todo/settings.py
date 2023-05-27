21f41674d30cccbd0caa6ff945a80e3c3848473e
nameiswaiyinsin <waiyinsin@gmail.com>
2023-05-27 21:05:38 +0000

    Add *.sqlite3 and __pycache__/ to gitignore and prepare to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import os
+import dj_database_url
+import env
+
+
 """
 Django settings for django_todo project.
 
@@ -12,10 +18,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 
 
-from pathlib import Path
-import os
-import dj_database_url
-import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
